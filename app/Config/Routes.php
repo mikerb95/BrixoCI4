e5a071b22a8a7f44c3ac7c00d32f4db288f0ec03
Jerson7molina <molinajerson885@gmail.com>
2025-11-24 20:35:06 -0500

    agregue el controlador Productos con método index para mostrar la vista

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,9 +10,3 @@ $routes->get('/logout', 'Home::logout');
 
 
 $routes->match(['get', 'post'], '/register', 'Auth::register');
-
-$routes->get('/productos', 'Productos::index');
-$routes->get('/mapa', 'Mapa::index');
-$routes->get('/profesional/(:num)', 'Profesional::ver/$1');
-$routes->get('/servicios', 'Servicios::index');
-$routes->get('/servicio/(:num)', 'Servicios::detalle/$1');
