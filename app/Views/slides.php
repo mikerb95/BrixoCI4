bb3e846394510b36883ee002e68c7881497ac293
Mike Rodriguez <69970540+mikerb95@users.noreply.github.com>
2025-12-10 11:55:48 -0500

    ajuste en visualizacion de slides

--- app/Views/slides.php
+++ app/Views/slides.php
@@ -7,7 +7,8 @@
     <title>Presentación - Brixo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
-        html, body {
+        html,
+        body {
             height: 100vh;
             margin: 0;
             padding: 0;
